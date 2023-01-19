27397ef905d4408ba25209013bda1a63f2acaf82
Carlos10p <113153116+Carlos10p@users.noreply.github.com>
2023-01-18 19:03:00 -0600

    Update menu.php
    cambio menu

--- menu.php
+++ menu.php
@@ -1,3 +1,30 @@
+<?php
+	require_once '.'.DIRECTORY_SEPARATOR.'motor'.DIRECTORY_SEPARATOR.'conexion.php';
+	require_once '.'.DIRECTORY_SEPARATOR.'sesions.php';
+
+	$conexion = new conexion();
+
+	$sql="CALL sp_charpierre_listaModulos();";
+
+	$resultado= $conexion->realizaConsulta($sql);
+
+	$num=$conexion->bd_cuentaRegistros($resultado);
+	$res= array();
+
+	if($num>0){
+		
+		while($row = $conexion->bd_dameRegistro($resultado)){
+			
+			$datos = array(
+							'nombre'=>$row['nombre'],
+							'ruta'=>$row['ruta'],
+							'Icono'=>$row['Icono']
+						);
+			array_push($res,$datos);
+		}
+	}
+	
+?>
 <!-- [ navigation menu ] start -->
 <nav class="pcoded-navbar menu-light ">
 		<div class="navbar-wrapper  ">
@@ -12,27 +39,22 @@
 					</div>
 					<div class="collapse" id="nav-user-link">
 						<ul class="list-unstyled">
-							<li class="list-group-item"><a href="user-profile.html"><i class="feather icon-user m-r-5"></i>Ver perfil</a></li>
-							<li class="list-group-item"><a href="#!"><i class="feather icon-settings m-r-5"></i>Ajustes</a></li>
 							<li class="list-group-item"><a href="?request='logOff'"><i class="feather icon-log-out m-r-5"></i>Salir</a></li>
 						</ul>
 					</div>
 				</div>
-				
 				<ul class="nav pcoded-inner-navbar ">
 					<li class="nav-item pcoded-menu-caption">
 					    <label>Navigation</label>
 					</li>
-					<li class="nav-item">
-					    <a href="index.html" class="nav-link "><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext">Dashboard</span></a>
-					</li>
-					<li class="nav-item pcoded-hasmenu">
-					    <a href="#!" class="nav-link "><span class="pcoded-micon"><i class="feather icon-layout"></i></span><span class="pcoded-mtext">Page layouts</span></a>
-					    <ul class="pcoded-submenu">
-					        <li><a href="layout-vertical.html" target="_blank">Vertical</a></li>
-					        <li><a href="layout-horizontal.html" target="_blank">Horizontal</a></li>
-					    </ul>
-					</li>
+					<?php
+						for($i=0;$i<count($res);$i++){
+							echo '
+								<li class="nav-item">
+									<a href="?page='. $res[$i]['ruta'].'" class="nav-link "><span class="pcoded-micon"><i class="feather icon-'. $res[$i]['Icono'].'"></i></span><span class="pcoded-mtext">'. $res[$i]['nombre'].'</span></a>
+								</li>';	
+						}
+					?>
 				</ul>
 			</div>
 		</div>
